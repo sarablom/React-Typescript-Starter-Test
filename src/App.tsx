dd0c6a5d10281a2ff40e25530a8c719a18886d53
Daniel Persson <persson.daniel.1990@gmail.com>
2021-09-18 14:52:39 +0200

    Minor code refactoring

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,7 @@
 import React from 'react';
 
 import './App.css';
-import { Header } from './components/layout/Header';
+import { Header } from './components/layout/header/Header';
 
 function App() {
   return (
